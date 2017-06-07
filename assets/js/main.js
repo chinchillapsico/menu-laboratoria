37b3b86639ee262b31463eb74af6f16c12674964
chinchillapsico <coraline.colous@gmail.com>
2017-06-06 22:45:05 -0400

    avance

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var nav = false;
+
+function show_nav(id){
+var navbar = document.getElementById(id);
+
+if(nav == false){
+navbar.style.display = "block";
+nav = true;
+}else{
+navbar.style.display = "none";
+nav = false;
+}
+}
